a0db31b9385cfe1180b7d4028f0ce337b7b9464d
SantiagoStrauss <ds.ramos@uniandes.edu.co>
2024-12-18 09:31:04 -0500

    s5

--- regis.py
+++ regis.py
@@ -11,7 +11,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import logging
-from typing import Optional
+from typing import List, Optional
 from dataclasses import dataclass
 from contextlib import contextmanager
 import traceback
